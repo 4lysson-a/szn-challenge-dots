d8838dc03c1658aa8a4814d550edcd2e1747ceb3
4lysson-a <4ly.alcantara@gmail.com>
2024-07-31 22:41:12 -0300

    chore: add transition animation to MarkView component

--- src/components/layout/MarkView.tsx
+++ src/components/layout/MarkView.tsx
@@ -19,6 +19,7 @@ function MarkComponent() {
         top: String(mark.y + "px"),
         transform: "scale(0)",
         animation: "show 0.1s ease forwards",
+        transition: '0.5s cubic-bezier(.83,.03,.04,1.06) all',
         boxShadow:
           "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px",
       }}
